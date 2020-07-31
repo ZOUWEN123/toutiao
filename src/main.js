0707872bbfd430581a254fb9cb9d1163c331c12c
zw <15179783291@163.com>
2020-07-31 21:27:17 +0800

    发送头条的tab栏的实现

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import router from "./router";
 import store from "./store";
 // import axios from "axios";
 import axios from "./common/myaxios";
+//引入element-ui组件库
+import { Message } from "element-ui";
 
+Vue.prototype.$message = Message;
 Vue.config.productionTip = false;
 Vue.prototype.$axios = axios;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,45 @@
       <div class="home-content-left">
         <Navigator></Navigator>
       </div>
-      <div class="home-content-middle">中间</div>
+      <div class="home-content-middle">
+        <div class="content-middle-top">
+          <section class="middle-top-tab">
+            <section
+              v-for="item in tabs"
+              @click="handleTabChange(item.type)"
+              :key="item.id"
+              :class="['top-tab-item', { active: activeTab === item.type }]"
+            >
+              {{ item.text }}
+            </section>
+          </section>
+        </div>
+        <div class="content-middle-mid">
+          <section v-show="activeTab == 'tt'">
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="写点什么吧"
+            ></textarea>
+            <section class="mid-bottom">
+              <section class="left">
+                <span class="title">图片</span>
+              </section>
+              <section class="right" @click.stop="publishTT">发布</section>
+            </section>
+          </section>
+          <section class="article-content" v-show="activeTab == 'article'">
+            <input class="article-input" v-model="title" type="text" />
+            <vue-editor id="editor" v-model="html_content"></vue-editor>
+            <section class="article-publish">
+              <span class="publish-title" @click="publishArticle">发布</span>
+            </section>
+          </section>
+        </div>
+        <div class="content-middle-bottom"></div>
+      </div>
       <div class="home-content-right">右边</div>
     </div>
   </div>
@@ -16,6 +54,7 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
+import { VueEditor } from "vue2-editor";
 import Navigator from "../components/Navigator";
 
 export default {
@@ -23,7 +62,78 @@ export default {
   components: {
     // HelloWorld,
     Navigator,
+    VueEditor,
   },
+  data() {
+    return {
+      content: "", //头条里面的内容
+      //tab栏数据
+      tabs: [
+        { id: 1, text: "发微头条", type: "tt" },
+        { id: 1, text: "写文章", type: "article" },
+      ],
+      title: "", //标题
+      html_content: "", //富文本编辑器内容
+      //当前激活的tab
+      activeTab: "tt",
+    };
+  },
+  //方法集合
+  methods: {
+    publishArticle: function() {
+      let title = this.title;
+      let html_content = this.html_content;
+      if (!title || !html_content) {
+        // 内容为空的情况下
+        // todo  换成 elementUI message
+        this.$message({
+          message: "输入不能为空！",
+          type: "warning",
+        });
+        return false;
+      }
+      this.$axios
+        .post("/createArticle", {
+          content: html_content,
+          img: "",
+          title: title,
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => console.log(err));
+    },
+    // auth_token KwiVWLCxXax3rRcVsmgX7shQGhtBtXnS
+    publishTT: function() {
+      let content = this.content;
+      if (!content) {
+        // 内容为空的情况下
+        // todo  换成 elementUI message
+        this.$message({
+          message: "输入不能为空！",
+          type: "warning",
+        });
+        return false;
+      }
+      this.$axios
+        .post("/createTT", {
+          content: content,
+          imgs: "",
+          // oauth_token:"KwiVWLCxXax3rRcVsmgX7shQGhtBtXnS"
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      this.content = "";
+    },
+    handleTabChange: function(activeTab) {
+      this.activeTab = activeTab;
+    },
+  },
+
   //   mounted: function () {
   //     // http://tt.linweiqin.com/api/tt/getArticles?lastid=0&&type="TT"&&page=1&&number=20
   //     this.$axios
@@ -64,7 +174,7 @@ export default {
   },
 };
 </script>
-<style lang='less' scoped>
+<style lang="less" scoped>
 .home-header {
   height: 30px;
   background-color: #000000;
@@ -85,6 +195,75 @@ export default {
 
   .home-content-middle {
     flex: 3;
+    .content-middle-top {
+      section.middle-top-tab {
+        display: flex;
+        section.top-tab-item {
+          height: 50px;
+          width: 150px;
+          line-height: 50px;
+          text-align: center;
+        }
+        .active {
+          color: #b1e5ff;
+          border-bottom: 2px solid #f1545a;
+        }
+      }
+    }
+    .content-middle-mid {
+      section {
+        textarea {
+          width: 100%;
+        }
+
+        section.mid-bottom {
+          display: flex;
+          height: 40px;
+          align-items: center;
+          justify-content: space-between;
+          section.left {
+            span.title {
+            }
+          }
+          section.right {
+            height: 30px;
+            line-height: 30px;
+            text-align: center;
+            width: 100px;
+            background-color: #ea4245;
+            color: white;
+          }
+        }
+      }
+      .article-content {
+        margin-top: 10px;
+        input.article-input {
+          width: 100%;
+          height: 30px;
+        }
+
+        #editor {
+          width: 100%;
+        }
+
+        .article-publish {
+          display: flex;
+          justify-content: flex-end;
+          .publish-title {
+            display: inline-block;
+            width: 120px;
+            height: 30px;
+            line-height: 30px;
+            text-align: center;
+            color: white;
+            background-color: #ea4245;
+          }
+        }
+      }
+    }
+
+    .content-middle-bottom {
+    }
   }
 
   .home-content-right {
